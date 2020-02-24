a1e041391a78ffa0571a6495a54fd8556974b6ed
Ramesh_baswa <baswaramesh1921@gmail.com>
2020-02-24 19:13:14 +0530

    changes in faq

--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faqs.component.css']
 })
 export class FaqsComponent implements OnInit {
+  loadScripts() {
+    const dynamicScripts = [
+      '../../../assets/js/main.js'
+    ];
+    for (let i = 0; i < dynamicScripts.length; i++) {
+      const node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
 
+ 
+
+  }
   constructor() { }
 
   ngOnInit() {
